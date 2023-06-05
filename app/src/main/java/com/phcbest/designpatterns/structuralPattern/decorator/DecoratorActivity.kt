6aede058c73b94b15a31c6c2cf8adae09bff40be
phcbest <phcbest2017@outlook.com>
2023-06-05 22:19:00 +0800

    上传装饰器模式的代码

--- app/src/main/java/com/phcbest/designpatterns/structuralPattern/decorator/DecoratorActivity.kt
+++ app/src/main/java/com/phcbest/designpatterns/structuralPattern/decorator/DecoratorActivity.kt
@@ -1,11 +1,10 @@
 package com.phcbest.designpatterns.structuralPattern.decorator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
-import androidx.core.content.res.ResourcesCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.phcbest.designpatterns.R
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.phcbest.designpatterns.databinding.ActivityDecoratorBinding
 
 class DecoratorActivity : AppCompatActivity() {
@@ -17,8 +16,27 @@ class DecoratorActivity : AppCompatActivity() {
         initView()
     }
 
-    private var dataSet = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-
     private fun initView() {
+        binding.tvEnableDecorator.setOnClickListener(AntiShakeClickDecorator(2000L) {
+            Toast.makeText(this, "这个是带2000ms防抖的哦", Toast.LENGTH_SHORT).show()
+        })
+        binding.tvDisEnableDecorator.setOnClickListener {
+            Toast.makeText(this, "这个是不带防抖的哦", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    class AntiShakeClickDecorator(
+        private val delay: Long = 2000,
+        private val onClickListener: View.OnClickListener
+    ) : View.OnClickListener {
+        private var lastClickTime: Long = 0
+
+        override fun onClick(v: View?) {
+            val currentTime = System.currentTimeMillis()
+            if (currentTime - lastClickTime > delay) {
+                lastClickTime = currentTime
+                onClickListener.onClick(v)
+            }
+        }
     }
 }
