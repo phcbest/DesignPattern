31b7c9d7ec97add51777f19e3f3f3fa4c21bd4cf
phcbest <phcbest2017@outlook.com>
2023-06-13 22:19:57 +0800

    变种命令模式

--- app/src/main/java/com/phcbest/designpatterns/behaviorPattern/command/CommandDemo.kt
+++ app/src/main/java/com/phcbest/designpatterns/behaviorPattern/command/CommandDemo.kt
@@ -1,14 +1,31 @@
 package com.phcbest.designpatterns.behaviorPattern.command
 
 import android.content.Context
+import android.util.Log
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+private const val TAG = "CommandDemo"
 
 object CommandDemo {
-    fun doDemo(context: Context){
+    fun doDemo(context: Context) {
         val commandReceiver = CommandReceiver()
         val commandTimeImpl = CommandTimeImpl(commandReceiver)
         val commandDateImpl = CommandDateImpl(commandReceiver)
         val commandInvoker = CommandInvoker(commandDateImpl, commandTimeImpl)
         commandInvoker.executeToastDate(context)
         commandInvoker.executeToastTime(context)
+
+        //命令对象本身就完成了具体任务的命令模式
+        val executor: Executor = Executors.newSingleThreadExecutor()
+        val runnable = Runnable { Log.i(TAG, "do yourself in Runnable") }
+        executor.execute(runnable)
+        val thread: Thread = object : Thread() {
+            override fun run() {
+                super.run()
+                Log.i(TAG, "do yourself in Thread")
+            }
+        }
+        executor.execute(thread)
     }
 }
